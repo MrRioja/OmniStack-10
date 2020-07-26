60f52b11df17e3702d00bad99669c783967ce7b2
Luiz Rioja <lulyrioja@gmail.com>
2020-07-25 22:30:49 -0300

    Adição do CORS para acessar a API pela web

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -8,6 +9,7 @@ mongoose.connect(
   "mongodb+srv://omnistack10:omnistack10@cluster0.zj4el.mongodb.net/omnistack10?retryWrites=true&w=majority", { useUnifiedTopology: true, useNewUrlParser: true }
 );
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.listen(3333);

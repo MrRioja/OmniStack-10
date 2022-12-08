1a4b988ea691a62beb93a3d2f3beb7c26768b792
Luiz Rioja <lulyrioja@gmail.com>
2022-12-08 19:21:47 -0300

    chore: update stack navigator

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,35 +1,27 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
 
 import Main from "./Pages/Main";
 import Profile from "./Pages/Profile";
 
-const Routes = createAppContainer(
-  createStackNavigator(
-    {
-      Main: {
-        screen: Main,
-        navigationOptions: {
-          title: "DevRadar",
-        },
-      },
-      Profile: {
-        screen: Profile,
-        navigationOptions: {
-          title: "Perfil no Github",
-        },
-      },
-    },
-    {
-      defaultNavigationOptions: {
-        headerBackTitleVisible: false,
+const Stack = createStackNavigator();
+
+function Routes() {
+  return (
+    <Stack.Navigator
+      defaultScreenOptions={{
         headerTintColor: "#FFF",
-        headerStyle: {
-          backgroundColor: "#7D40E7",
-        },
-      },
-    }
-  )
-);
+        headerBackTitleStyle: false,
+        headerStyle: { backgroundColor: "#7D40E7" },
+      }}
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="DevRadar" component={Main} />
+      <Stack.Screen name="Profile" component={Profile} />
+    </Stack.Navigator>
+  );
+}
 
 export default Routes;

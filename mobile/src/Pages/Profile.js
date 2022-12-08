87e023a47c45d0d5dc0849c18392af96b2ca6015
Luiz Rioja <lulyrioja@gmail.com>
2022-12-08 19:19:29 -0300

    fix: get route githubUsername route param

--- mobile/src/Pages/Profile.js
+++ mobile/src/Pages/Profile.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { WebView } from "react-native-webview";
 
-function Profile({ navigation }) {
-  const githubUsername = navigation.getParam("github_username");
+function Profile({ route, navigation }) {
+  const { github_username } = route.params;
 
   return (
     <WebView
       style={{ flex: 1 }}
-      source={{ uri: `https://github.com/${githubUsername}` }}
+      source={{ uri: `https://github.com/${github_username}` }}
     />
   );
 }
